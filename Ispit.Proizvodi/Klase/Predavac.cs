d2af8fb3cce4f00dfb096b3d98a9a8604aa964ba
AntonijoTomic <antonijo.tomic12@gmail.com>
2024-07-11 18:51:29 +0200

    Add project files.

---
+++ Ispit.Proizvodi/Klase/Predavac.cs
@@ -0,0 +1,32 @@
+﻿using System;
+
+namespace Ispit.Proizvodi.Klase
+{
+    public class Predavac
+    {
+        public event PocniPisatiIspit Ispit;
+        public int Id { get; set; }
+        public string Ime { get; set; }
+        public string Prezime { get; set; }
+
+        public string ImePrezime
+        {
+            get { return $"{Ime} {Prezime}"; }
+        }
+
+        public void ZvoniZvono()
+        {
+            Console.WriteLine("Brrrrrrrrrrrrrrrrrrrrrrrrrrrrrr, ispit počinje.\n\n");
+            if(Ispit != null)
+            {
+                Ispit(DateTime.Now);
+            }
+      
+        }
+
+        public void IspitZaprimljen(Polaznik polaznik)
+        {
+            Console.WriteLine("Zaprimljen ispit od polaznika: {0}", polaznik.ImePrezime);
+        }
+    }
+}
---
+++ Ispit.Proizvodi/Program.cs
@@ -0,0 +1,44 @@
+﻿using Ispit.Proizvodi.Klase;
+using System;
+using System.Collections.Generic;
+
+namespace Ispit.Proizvodi
+{
+    public delegate void PocniPisatiIspit(DateTime vrijemePocetka);
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Predavac predavac = new Predavac()
+            {
+                Id=1,
+                Ime="Karlo",
+                Prezime="Markovic"
+            };
+
+            predavac.ZvoniZvono();
+
+            List<Polaznik> polaznici = new List<Polaznik>
+            {
+              new Polaznik  { Id = 1, Ime = "Antonijo", Prezime = "Tomic" },
+              new Polaznik { Id = 2, Ime = "Vlatka", Prezime = "Maric" },
+              new Polaznik { Id = 3, Ime = "Drago", Prezime = "Hodak" },
+              new Polaznik { Id = 4, Ime = "Ratko", Prezime = "Cosic" }
+            };
+
+            foreach(Polaznik p in polaznici)
+            {
+                p.OdgovoriNaPitanja(DateTime.Now);
+            }
+
+            polaznici[0].PredajOdgovoreNaPitanja();
+
+            predavac.IspitZaprimljen(polaznici[0]);
+
+
+            Console.ReadKey();
+        }
+    }
+}
+
+//
